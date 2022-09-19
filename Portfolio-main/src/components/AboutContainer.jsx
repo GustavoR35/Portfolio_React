1227592713eac1eadd7452c4749196ffe6394ff4
Gustavo Rodrigues <gugulara986@gmail.com>
2022-09-19 14:36:59 -0400

    Primeiro commit

---
+++ Portfolio-main/src/components/AboutContainer.jsx
@@ -0,0 +1,15 @@
+const AboutContainer = () => {
+  return (
+    <section className="about-container">
+      <h2>Sobre</h2>
+      <p>
+      Olá, tudo bem?
+Meu nome é Gustavo Rodrigues Neves, tenho 21 anos e estou graduando em Ciências da Computação na Faculdade Invest.
+Atualmente estou estagiando na área de suporte, localizado na SINFRA (Secretaria de Estado de Infraestrutura e Logística).
+      </p>
+    
+    </section>
+  );
+};
+
+export default AboutContainer;
